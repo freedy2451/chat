b50c24918a169e6bef9b185ae91c96cfe5d010a6
Lucy <b237237@gmail.com>
2023-03-11 21:56:26 +0800

    'v0.01'

---
+++ chat_record.py
@@ -0,0 +1,35 @@
+import os 
+
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding = 'utf-8-sig') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines        
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:    
+            new.append(person + ":" + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w', encoding = 'utf-8') as f: 
+        for line in lines:  
+            f.write(line + '\n')
+
+def main():
+    lines = read_file('./對話紀錄相關檔案/對話紀錄1/input.txt')
+    lines = convert(lines)
+    write_file('./對話紀錄相關檔案/對話紀錄1/output.txt', lines)
+    
+if __name__ == '__main__':
+    main()        
